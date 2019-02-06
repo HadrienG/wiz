558da2ca178cd81cbdf3bf2fa945ebc6bdae356c
Chaoui <antoine.druart@std.heh.be>
2019-02-06 16:11:28 +0100

    Adding in the "test_average_gc_by_frame()" a test with ch II of S288c

--- wiz/qc/gc.py
+++ wiz/qc/gc.py
@@ -19,19 +19,32 @@ from Bio.SeqUtils import GC
 #     return average
 
 
-def average_gc_by_frame(sequence, len_frame=5):
-    if frame > len(sequence):
-        raise ValueError("The size of the frame is superior of the sequence length.")
-    if frame <= 0:
-        raise ValueError("The size of the frame is negative or null.")
+def check_frame_size(sequence, len_frame):
     if len(sequence) == 0:
-        raise ValueError("The sequence is void")
-    average_gc_by_windows = []
-    for pos in range(0, len(sequence), len_frame):
-        average_gc = 0
-        if pos+frame < len(sequence):
-            average_gc = GC(sequence[pos:pos+frame])
-        else:
-            average_gc = GC(sequence[pos:])
-        average_gc_by_windows.append(average_gc)
-    return average_gc_by_windows
+        error = "The sequence is void"
+        raise ValueError(error)
+    if len_frame <= 0:
+        error = "The size of the frame is negative or null."
+        raise ValueError(error)
+    if len_frame > len(sequence):
+        error = "The size of the frame is superior of the sequence length."
+        raise ValueError(error)
+    return True
+
+
+def average_gc_by_frame(sequence, len_frame=5000):
+    if check_frame_size(sequence, len_frame):
+        average_gc_by_windows = []
+        for pos in range(0, len(sequence), len_frame):
+            average_gc = 0
+            if pos+len_frame < len(sequence):
+                average_gc = GC(sequence[pos:pos+len_frame])
+            else:
+                average_gc = GC(sequence[pos:])
+            average_gc_by_windows.append(average_gc)
+        return average_gc_by_windows
+
+
+def gc_histogram(sequence, len_frame=5000):
+    # coming soon
+    pass
--- wiz/test/test_gc.py
+++ wiz/test/test_gc.py
@@ -18,13 +18,24 @@ def test_average_gc():
         assert gc.bio_package_gc(test_seq) == test_value
 
 
-def test_average_gc_by_frame:
+def test_average_gc_by_frame():
+    ch2_Saccharomyces = ""
+    ch2_Saccharomyces_result = []
+    with open("test_gc.fasta", "r") as f:
+        _ = f.readline()
+        ch2_Saccharomyces = f.read()
+        ch2_Saccharomyces.replace("\n", "")
+    with open("test_gc.result", "r") as result:
+        ch2_Saccharomyces_result = [entry.replace("\n", "") for entry in result.readlines()]
+        ch2_Saccharomyces_result = [float(entry) for entry in ch2_Saccharomyces_result]
+    #   ch2_Saccharomyces_result = [float(entry.replace("\n","")) for entry in result.readlines()]   mistakes here
     test_sequences = [
         (Seq("GATCGATGGGCCTATATAGGATCGAAAATCTAACTTG", IUPAC.unambiguous_dna),
-            ["GATCG", "ATGGG", "CCTAT", "ATAGG", "ATCGA", "AAATC", "TAACT",
-                "TG"]),
+            [60, 60, 40, 40, 40, 20, 20,
+                50], 5),
         (Seq("RATSSATRRSSYTATATARRATYRAAAATY", IUPAC.unambiguous_dna),
-            ["RATSSATRRS", "SYTATATARR", "ATYRAAAATY"], 10)  # ,
+            [30, 10, 0], 10),
+        (Seq(ch2_Saccharomyces, IUPAC.unambiguous_dna), ch2_Saccharomyces_result)  # ,
             #  (Seq("RATSSATRRSSYTATATARRATYRAAAATY", IUPAC.unambiguous_dna), ValueError, 0),
             #  (Seq("RATSSATRRSSYTATATARRATYRAAAATY", IUPAC.unambiguous_dna), ValueError, -10),
             #  (Seq("", IUPAC.unambiguous_dna), ValueError, 10)
@@ -36,4 +47,3 @@ def test_average_gc_by_frame:
         else:
             test_seq, test_value = test
             assert gc.average_gc_by_frame(test_seq) == test_value
-        
